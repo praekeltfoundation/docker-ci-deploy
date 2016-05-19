0e7212024168e78a3a8df6ffe71dac38b0c0eade
Jamie Hewland <jhewland@gmail.com>
2016-05-19 10:33:58 +0200

    Use echo directly rather than via a script

--- docker_ci_deploy/tests/test_main.py
+++ docker_ci_deploy/tests/test_main.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import stat
-
 import pytest
 
 from subprocess import CalledProcessError
@@ -119,28 +117,21 @@ def test_cmd_error(capfd):
 
 
 class TestDockerCiDeployRunner(object):
-    @pytest.fixture(scope='session')
-    def echo_script(self, tmpdir_factory):
-        path = tmpdir_factory.mktemp('tmp').join('echo_script.sh')
-        path.write('#!/bin/sh\necho "$@"\n')
-        path.chmod(path.stat().mode | stat.S_IEXEC)
-        return str(path)
-
-    def test_defaults(self, capfd, echo_script):
+    def test_defaults(self, capfd):
         """
         When the runner is run with defaults, the image should be pushed.
         """
-        runner = DockerCiDeployRunner(executable=echo_script)
+        runner = DockerCiDeployRunner(executable='echo')
         runner.run('test-image')
 
         assert_output_lines(capfd, ['push test-image'])
 
-    def test_tags(self, capfd, echo_script):
+    def test_tags(self, capfd):
         """
         When tags are provided to the runner, the image should be tagged and
         each tag should be pushed.
         """
-        runner = DockerCiDeployRunner(executable=echo_script)
+        runner = DockerCiDeployRunner(executable='echo')
         runner.run('test-image', tags=['abc', 'def'])
 
         assert_output_lines(capfd, [
@@ -150,13 +141,13 @@ class TestDockerCiDeployRunner(object):
             'push test-image:def'
         ])
 
-    def test_tag_replacement(self, capfd, echo_script):
+    def test_tag_replacement(self, capfd):
         """
         When tags are provided to the runner and the provided image has a tag,
         that tag should be tagged with the new tag, and the the new tag should
         be pushed.
         """
-        runner = DockerCiDeployRunner(executable=echo_script)
+        runner = DockerCiDeployRunner(executable='echo')
         runner.run('test-image:abc', tags=['def'])
 
         assert_output_lines(capfd, [
@@ -164,12 +155,12 @@ class TestDockerCiDeployRunner(object):
             'push test-image:def'
         ])
 
-    def test_registry(self, capfd, echo_script):
+    def test_registry(self, capfd):
         """
         When a registry is provided to the runner, the image should be tagged
         with the registry and pushed.
         """
-        runner = DockerCiDeployRunner(executable=echo_script)
+        runner = DockerCiDeployRunner(executable='echo')
         runner.run('test-image', registry='registry.example.com:5000')
 
         assert_output_lines(capfd, [
@@ -177,12 +168,12 @@ class TestDockerCiDeployRunner(object):
             'push registry.example.com:5000/test-image'
         ])
 
-    def test_tags_and_registry(self, capfd, echo_script):
+    def test_tags_and_registry(self, capfd):
         """
         When tags and a registry are provided to the runner, the image should
         be tagged with both the tags and the registry and pushed.
         """
-        runner = DockerCiDeployRunner(executable=echo_script)
+        runner = DockerCiDeployRunner(executable='echo')
         runner.run('test-image:ghi', tags=['abc', 'def'],
                    registry='registry.example.com:5000')
 
@@ -193,12 +184,12 @@ class TestDockerCiDeployRunner(object):
             'push registry.example.com:5000/test-image:def'
         ])
 
-    def test_login(self, capfd, echo_script):
+    def test_login(self, capfd):
         """
         When login details are provided to the runner, a login request should
         be made and the image should be pushed.
         """
-        runner = DockerCiDeployRunner(executable=echo_script)
+        runner = DockerCiDeployRunner(executable='echo')
         runner.run('test-image', login='janedoe:pa$$word')
 
         assert_output_lines(capfd, [
@@ -206,13 +197,13 @@ class TestDockerCiDeployRunner(object):
             'push test-image'
         ])
 
-    def test_registry_and_login(self, capfd, echo_script):
+    def test_registry_and_login(self, capfd):
         """
         When a registry and login details are provided to the runner, the image
         should be tagged with the registry and a login request should be made
         to the specified registry. The image should be pushed.
         """
-        runner = DockerCiDeployRunner(executable=echo_script)
+        runner = DockerCiDeployRunner(executable='echo')
         runner.run('test-image', registry='registry.example.com:5000',
                    login='janedoe:pa$$word')
 
@@ -223,14 +214,14 @@ class TestDockerCiDeployRunner(object):
             'push registry.example.com:5000/test-image'
         ])
 
-    def test_all_options(self, capfd, echo_script):
+    def test_all_options(self, capfd):
         """
         When tags, a registry, and login details are provided to the runner,
         the image should be tagged with the tags and registry, a login request
         should be made to the specified registry, and the tags should be
         pushed.
         """
-        runner = DockerCiDeployRunner(executable=echo_script)
+        runner = DockerCiDeployRunner(executable='echo')
         runner.run('test-image:tag', tags=['latest', 'best'],
                    registry='registry.example.com:5000',
                    login='janedoe:pa$$word')
@@ -244,41 +235,39 @@ class TestDockerCiDeployRunner(object):
             'push registry.example.com:5000/test-image:best'
         ])
 
-    def test_dry_run(self, capfd, echo_script):
+    def test_dry_run(self, capfd):
         """
         When running in dry-run mode, the expected commands should be logged
         and no other output should be produced as no subprocesses should be
         run.
         """
-        runner = DockerCiDeployRunner(executable=echo_script, dry_run=True)
+        runner = DockerCiDeployRunner(dry_run=True)
         logs = []
         runner.logger = lambda *args: logs.append(' '.join(args))
         runner.run('test-image:tag', tags=['latest'])
 
         expected = [
-            'tag test-image:tag test-image:latest',
-            'push test-image:latest'
+            'docker tag test-image:tag test-image:latest',
+            'docker push test-image:latest'
         ]
-        expected = ['%s %s' % (echo_script, s,) for s in expected]
         assert logs == expected
 
         assert_output_lines(capfd, [], [])
 
-    def test_dry_run_obfuscates_password(self, capfd, echo_script):
+    def test_dry_run_obfuscates_password(self, capfd):
         """
         When running in dry-run mode and login details are provided, the user's
         password should not be logged.
         """
-        runner = DockerCiDeployRunner(executable=echo_script, dry_run=True)
+        runner = DockerCiDeployRunner(dry_run=True)
         logs = []
         runner.logger = lambda *args: logs.append(' '.join(args))
         runner.run('test-image', login='janedoe:pa$$word')
 
         expected = [
-            'login --username janedoe --password <password>',
-            'push test-image'
+            'docker login --username janedoe --password <password>',
+            'docker push test-image'
         ]
-        expected = ['%s %s' % (echo_script, s,) for s in expected]
         assert logs == expected
 
         assert_output_lines(capfd, [], [])
