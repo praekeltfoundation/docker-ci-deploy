3cb4bf7d47f2819729bc1cfc08eb29150bd529bb
Jamie Hewland <jhewland@gmail.com>
2016-05-20 13:06:04 +0200

    Bump version post-release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='docker-ci-deploy',
-    version='0.1.2',
+    version='0.1.3-dev',
     license='MIT',
     url='https://github.com/praekeltfoundation/docker-ci-deploy',
     description='Python script to help push Docker images to a registry using '
